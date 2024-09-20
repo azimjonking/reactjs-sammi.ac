2d9f39960d9122b27721ad6f35727e40b3b24602
azimjonking <azimjonjalilov1301@mail.ru>
2024-09-20 11:55:19 +0300

    jsx preprotsessor

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const el = <h1>Hello world</h1>;
+// const el = React.createElement("h1", null, "Hello world");
+
+const name = "Azimjon";
+
+const el = (
+  <div>
+    <h1 className="greeting" tabIndex={1}>
+      Hello: {name}
+    </h1>
+    <input type="text" />
+    <button>Click</button>
+  </div>
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(el);
