9237f22ac093bdb4140a39ac04c3e4fd7cca6bd8
azimjonking <azimjonjalilov1301@mail.ru>
2024-09-21 11:50:40 +0300

    components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,40 @@
 import "./App.css";
 
+const Header = () => {
+  const text = "Azimjon";
+  const getText = (text) => {
+    return (
+      <div>
+        <h1>Hello {text}</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          voluptates sunt aut odit laborum libero laboriosam tempore in rem
+          officia.
+        </p>
+      </div>
+    );
+  };
+  return <div>{getText(text)}</div>;
+};
+
+const Field = () => {
+  const placeholder = "Typing...";
+  const typeInput = "text";
+  return <input type={typeInput} placeholder={placeholder} />;
+};
+
+const Button = () => {
+  const user = false;
+  const login = "Login";
+  return <button>{user ? "logout" : login}</button>;
+};
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Field />
+      <Button />
     </div>
   );
 }
